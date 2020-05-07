4e1b1d881da7265d25671b3d03e71e87f81fc04d
vincent <vinzmass@gmail.com>
2020-05-07 19:40:17 +0200

    Update parameters files

--- core/Config/ParametersInterface.php
+++ core/Config/ParametersInterface.php
@@ -8,6 +8,10 @@ interface ParametersInterface
     const KEY_DATABASE_USER = 'database_user';
     const KEY_DATABASE_PASSWORD = 'database_password';
     const KEY_IS_DEBUG = 'is_debug';
+    const KEY_EMAIL_CONTACT = 'email_contact';
+    const KEY_CAPTCHA_PUBLIC_KEY = 'captcha_public_key';
+    const KEY_CAPTCHA_SECRET_KEY = 'captcha_secret_key';
+    const KEY_GOOGLE_MAP = 'google_map_key';
 
     /**
      * @return Parameter[]
--- src/Config/Parameters.php
+++ src/Config/Parameters.php
@@ -17,6 +17,10 @@ class Parameters implements ParametersInterface
             new Parameter(self::KEY_DATABASE_USER, 'root'),
             new Parameter(self::KEY_DATABASE_PASSWORD, ''),
             new Parameter(self::KEY_IS_DEBUG, true),
+            new Parameter(self::KEY_EMAIL_CONTACT, 'vinzmass@gmail.com'),
+            new Parameter( self::KEY_CAPTCHA_PUBLIC_KEY, '6Leh4-kUAAAAAOspZuwp7wj1a0xAnqD9YKYCN-eA'),
+            new Parameter( self::KEY_CAPTCHA_SECRET_KEY, '6Leh4-kUAAAAAJOHUwnl6_p9KkyG8qCtdIYKY7NR'),
+            new Parameter( self::KEY_GOOGLE_MAP, 'AIzaSyCPo0NaEvefJ4N6U1WfCLqxuvhX_Fnl4gc'),
         ];
     }
 }
