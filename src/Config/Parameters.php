4ef5414157e1cef1db01619252def54c4f928411
vincent <vinzmass@gmail.com>
2020-05-09 18:24:33 +0200

    Update Parameters.php

--- src/Config/Parameters.php
+++ src/Config/Parameters.php
@@ -20,7 +20,7 @@ class Parameters implements ParametersInterface
             new Parameter(self::KEY_EMAIL_CONTACT, 'vinzmass@gmail.com'),
             new Parameter( self::KEY_CAPTCHA_PUBLIC_KEY, '6Leh4-kUAAAAAOspZuwp7wj1a0xAnqD9YKYCN-eA'),
             new Parameter( self::KEY_CAPTCHA_SECRET_KEY, '6Leh4-kUAAAAAJOHUwnl6_p9KkyG8qCtdIYKY7NR'),
-            new Parameter( self::KEY_GOOGLE_MAP, 'AIzaSyCPo0NaEvefJ4N6U1WfCLqxuvhX_Fnl4gc'),
+            new Parameter( self::KEY_GOOGLE_MAP, 'AIzaSyCPo0NaEvefJ4N6U1WfCLqxuvhX_Fnl4gc')
         ];
     }
 }
