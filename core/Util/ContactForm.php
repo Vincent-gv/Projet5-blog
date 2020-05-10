7c90cda452e6c0ebcdfd8b071904240958a11d06
vincent <vinzmass@gmail.com>
2020-05-10 23:07:26 +0200

    Update ContactForm.php and form templates

--- core/Util/ContactForm.php
+++ core/Util/ContactForm.php
@@ -9,17 +9,17 @@ use Core\Config\ParametersInterface;
 
 abstract class ContactForm
 {
-    static public function getContactForm(string $email, string $subject, string $message): string
+    static public function getContactForm(string $name, string $email, string $subject, string $message): string
     {
         $recipient = ParameterManager::getParameter(ParametersInterface::KEY_EMAIL_CONTACT)->getValue();
         $headers = 'MIME-Version: 1.0' . "\r\n"
             . 'Content-type: text/html; charset=utf-8' . "\r\n"
             . 'From: ' . $email . "\r\n"
             . 'Reply-to:' . $email . '' . "\r\n";
-        $content = '<p><strong>Message</strong>: ' . $message . '</p>';
+        $content = '<p><strong>Expéditeur : </strong>' . $name . '</p><p><strong>Email : </strong>' . $email . '</p><p><strong>Objet : </strong>' . $subject . '</p><p><strong>Message : </strong>' . $message . '</p>';
         FlashBag::addFlash('Merci ! Votre message a bien été envoyé.', 'success');
 
-        return @mail($recipient, 'Nouveau message : ' . $subject . '', $content, $headers);
+        return @mail($recipient, 'Nouveau message Blog : ' . $subject . '', $content, $headers);
     }
 
 }
